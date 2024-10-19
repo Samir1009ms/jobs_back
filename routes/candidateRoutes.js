460320fbc5d9fb2572bd9ba6b8ec9e6fc8640c60
Samir1009ms <yusifovs1009@gmail.com>
2024-10-19 14:50:02 +0400

    update

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const router = express.Router();
-const candidateController = require('../controllers/candidateController');
+import express from 'express';
+import candidateController from '../controllers/candidateController';
 
+const router = express.Router();
 // Candidate Routes
 router.post('/', candidateController.createCandidate);
 router.get('/', candidateController.getAllCandidates);
